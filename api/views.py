6bbb83e76d406ce64baa51dbde6be56b0bffc99b
IanAlves <igaps.eng17@uea.edu.br>
2020-09-23 00:10:36 -0400

    update

--- api/views.py
+++ api/views.py
@@ -3,15 +3,19 @@ from django.contrib.auth.models import User
 import json
 
 def check_user(request):
-    print(request)
+    print(request.body)
     if request.method=='POST':
-        json_data=json.loads(request.POST.get(['data']))
-        print('Json:', json_data)
+        print("a1")
+        json_data = json.loads(request.body.decode()) 
+        print("a2")
+        # print('Json:', json_data)
         user = User.objects.get(username=json_data['username'])
+        print("a3")
         
-        if user.validate_password(json_data['password']) is None:
+        if user.check_password(json_data['password']):
             return JsonResponse({'message': True})
         else:
             return JsonResponse({'message': False})
+        print("a4")
     return JsonResponse({'message': False})
 
