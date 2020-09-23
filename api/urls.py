9b297df9fe5e526e42fa7b97a5175ee78b151c90
Carlos hagge <carloshaggecunha@gmail.com>
2020-09-22 22:30:00 -0400

    a

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,34 @@
 from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
 from rest_framework import routers, serializers, viewsets
 
 from . import views
 from . import models
+from .permissions import IsStaffOrTargetUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('password', 'username',)
+        write_only_fields = ('password',)
+        read_only_fields = ('is_staff', 'is_superuser', 'is_active', 'date_joined',)
+ 
+    def restore_object(self, attrs, instance=None):
+        # call set_password on user object. Without this
+        # the password will be stored in plain text.
+        user = super(UserSerializer, self).restore_object(attrs, instance)
+        user.set_password(attrs['password'])
+        return user
+
+class UserView(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    model = User
+    
+    def get_permissions(self):
+        # allow non-authenticated user to create via POST
+        return (AllowAny() if self.request.method == 'POST'
+                else IsStaffOrTargetUser()),
 
 class PostagemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -29,6 +55,7 @@ class ComentarioViewSet(viewsets.ModelViewSet):
 router = routers.DefaultRouter()
 router.register(r'postagens', PostagemViewSet)
 router.register(r'comentarios', ComentarioViewSet)
+router.register(r'accounts', UserView, 'list')
 
 
 urlpatterns = [
